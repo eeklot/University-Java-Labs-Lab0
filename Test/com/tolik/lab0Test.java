79b8b62e26a642ffc8eb133e3fe43c880f8b1385
Tolik Borkivskiy <tolikborkstudy@gmail.com>
2018-09-28 00:33:46 +0300

    Lab without two last tasts

---
+++ Test/com/tolik/lab0Test.java
@@ -0,0 +1,164 @@
+package com.tolik;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class lab0Test {
+    public lab0 obj = new lab0();
+
+    ///////////////////////////////////////////////////
+    // check : function that returns area of rect
+    // exception provider
+    @DataProvider
+    public Object[][] areaTaskException(){
+        return new Object[][] {
+                {-1000,1},
+                {-500,0}
+        };
+    }
+    // exceprion provider test
+    @Test(
+            expectedExceptions= IllegalArgumentException.
+            class,dataProvider = "areaTaskException"
+    )
+    public void areaException(float side,float expected){
+        assertEquals(obj.rectArea(side),expected);
+    }
+
+    @DataProvider
+    public Object[][] areaTaskProvider(){
+        // first - side of reckt and second the square
+        // actual,  expected
+        return new Object[][] {
+                {0,0},
+                {1,1},
+                {2,4},
+                {3,9}
+        };
+    }
+    @Test(dataProvider = "areaTaskProvider")
+    public void areaTest(float side_of_rect, float exp){
+        // actual then expected
+        assertEquals(obj.rectArea(side_of_rect),exp);
+    }
+
+    ///////////////////////////////////////////////
+    // check : find full tons in mass
+    @DataProvider
+    public Object[][] fullTonsProvider(){
+        // mass,    full tons
+        // actual,  expected
+        return new Object[][] {
+                {1000,1},
+                {1600,1},
+                {2100,2},
+                {500,0},
+                {0,0}
+        };
+    }
+    @Test(dataProvider = "fullTonsProvider")
+    public void fullTonsTest(int mass_to_chack,int exp){
+        assertEquals(obj.fullTons(mass_to_chack),exp);
+    }
+    ////////////////////////////////////////////////////
+    // check : function isEquals
+    @DataProvider
+    public Object[][] isEqualsProvider(){
+        return new Object[][] {
+                //ectual value - value Expected
+                {0,true},
+                {1,false},
+                {2,true},
+                {3,false},
+                {4,true},
+                {5,false},
+                {-10,true},
+                {-5,false}
+        };
+    }
+    @Test(dataProvider = "isEqualsProvider")
+    public void isEqualsTest(int value,boolean exp){
+        assertEquals(obj.isEquals(value),exp);
+    }
+    ////////////////////////////////////////////////
+    // test function : changeValue
+    @DataProvider
+    public Object[][] changeValueProvider(){
+        // actual , expected
+        return new Object[][] {
+                {0,1},
+                {1,2},
+                {10,11},
+                {-1,-3},
+                {-2,-4}
+        };
+    }
+    @Test(dataProvider = "changeValueProvider")
+    public void changeValue(int value,int exp){
+        assertEquals(obj.changeValue(value),exp);
+    }
+    ///////////////////////////////////////////////
+    // test function : examinate
+    @DataProvider
+    public Object[][] examinateProvider(){
+        // value ,expected res
+        return new Object[][] {
+                {1,"плохо"},
+                {2,"неудовлетворительно"},
+                {3,"удовлетворительно"},
+                {4,"хорошо»"},
+                {5,"отлично"},
+                {6,"ошибка"},
+                {0,"ошибка"},
+                {-1,"ошибка"}
+        };
+    }
+    @Test(dataProvider = "examinateProvider")
+    public void examinateTest(int value_to_pass,String exp){
+        assertEquals(obj.examinate(value_to_pass),exp);
+    }
+    ///////////////////////////////////////////////////////
+    // exp function : check
+    @DataProvider
+    public Object[][] expProvider(){
+        // function arg : X and N, exp value
+        return new Object[][] {
+                //X,N,exp
+                {1,0,1},
+                {1,1,2},
+                {1,2,2.5},
+                {1,3,2.666666},
+                {1,4,2.708333},
+                {2,1,3},
+                {2,2,4},
+                {2,3,4.333333},
+                {3,1,4},
+                {3,2,5.5}
+        };
+    }
+    @Test(dataProvider="expProvider")
+    public void expTest(double X,int N,double exp){
+        assertEquals(obj.exp(X,N), exp,0.000001);
+    }
+    /////////////////////////////////////////////////
+    // check function : linesIn
+    @DataProvider
+    public Object[][] linesInProvider(){
+        // line A, line B, exp
+        return new Object[][] {
+                {1,1,1},
+                {2,1,2},
+                {2,0.5f,4},
+                {1,0.2f,5},
+                {1,0.1f,10}
+        };
+    }
+    @Test(dataProvider = "linesInProvider")
+    public void linesInTest(float line_a,float line_b,int exp){
+        assertEquals(obj.linesIn(line_a,line_b),exp);
+    }
+    ///////////////////////////////////////////////////////////
+}
+
---
+++ src/com/tolik/lab0.java
@@ -0,0 +1,97 @@
+package com.tolik;
+
+public class lab0 {
+    // Begin 2 task
+    // return the area of reckt
+    public float rectArea(float side){
+        if( side < 0)
+            throw new IllegalArgumentException();
+        return side*side;
+    }
+    // Integer 2 task
+    // finde full tons in kilograms
+    public int fullTons(int mass)
+    {
+        if (mass < 0)
+            throw new IllegalArgumentException();
+        return mass/1000;
+    }
+    // Boolean 2 task
+    // value is int
+    // if value is equals or not
+    public boolean isEquals(int value){
+        return value % 2 == 0;
+    }
+    // If 2 task
+    //  we have int value
+    //  if value more then zero then increase by one
+    //  else decrease by two
+    public int changeValue(int value) {
+        return value >= 0 ? value + 1 : value - 2;
+    }
+    // Case 2 task
+    // 1 — «плохо»,
+    // 2 — «неудовлетворительно»,
+    // 3 — «удовлетворительно»,
+    // 4 — «хорошо»,
+    // 5 — «отлично»
+    // if value out of rang then return error
+    public String examinate(int value){
+        switch(value){
+            case 1 : return "плохо";
+            case 2 : return "неудовлетворительно";
+            case 3 : return "удовлетворительно";
+            case 4 : return "хорошо»";
+            case 5 : return "отлично";
+            default: return "ошибка";
+        }
+    }
+    // For 2 task
+    // we have X = real and N > 0
+    // find the result of expresion :
+    // 1 + X + X/2! + ... + X/N!
+    public double exp(double X, int N){
+        // sum is 1 becouce we did zero step by hands
+        double sum = 1;
+        long f = 1; // f is f!
+        for(int i = 1; i <= N; ++i){
+            f *= i;
+            sum += X/f;
+        }
+        return sum;
+    }
+    // While 2
+    // find how many lines B in A line
+    // if A > B
+    public int linesIn(float line_a, float line_b){
+        int count = 0;
+        double temp = 0;
+        while(temp < line_a){
+            temp += line_b;
+            ++count;
+        }
+        return count;
+    }
+    // Array 2
+    // int array
+    // return line numb of last value of array for which is true expresion array[1] < array[k] < array [10]
+    // else return 0
+    public int arrayTask(int[] array){
+        for(int i = array.length - 2; i >= 1; --i)
+            // the value i is not 0 and array.langth becouce in thet way the expresion will never be true
+            if( array[0] < array[i] && array[i] < array[ array.length-1 ])
+                return i+1;
+
+        return 0;
+    }
+    // Matrix 2
+    // change raws of matrix
+    // k1 to k2
+    public void changeRaws(float[][] mtrx,int k1, int k2){
+        for(int i = 0; i < mtrx.length; ++i){
+            float temp = mtrx[k1][i];
+            mtrx[k1][i] = mtrx[k2][i];
+            mtrx[k2][i] = temp;
+        }
+    }
+}
